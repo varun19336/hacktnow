b9ef6aacc8096b16138a38b96f7938f29f6e4280
varun19336 <56352363+varun19336@users.noreply.github.com>
2019-10-10 19:49:15 +0530

    Update 26.c

--- 26.c
+++ 26.c
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 
 int main() {
+    
+    
+    
+    
     int x;
     scanf("%d",&x); 
     if((x<<31)== 0){
@@ -14,4 +18,9 @@ int main() {
     }
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
+
+
+
+
+
 }
